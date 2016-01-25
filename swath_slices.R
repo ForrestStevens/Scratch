52a7638c5fdbaa6936b5627ae95e0c7b075973bf
Forrest R. Stevens <ForrestStevens@users.noreply.github.com>
2016-01-25 09:43:00 -0500

    Update swath_slices.R
    Made a reproducible example from original, census-specific code.

--- swath_slices.R
+++ swath_slices.R
@@ -1,25 +1,28 @@
 ##	Thresholds for N and S transect:
-##		6451870 N
-##		2317627 S
+##		332140 N
+##		330460 S
 ##
-##	Northern Hu point:
-##		1525348.068, 6113380.046
+##	Northern transect point:
+##		181000, 331500
 ##
-##	Southern Hu point:
-##		-672622.094, 3178865.248
+##	Southern transect point:
+##		180000, 330500
 
-setwd("C:/tmp/CHN/")
+library(sp)
+library(rgeos)
+
+data(meuse.grid)
 
 ##	Slice width in meters:
-slice_width <- 10000
+slice_width <- 500
 
 
 ##	Calculate angle of line for Northern Hu point:
-x1 <- -672622.094
-y1 <- 3178865.248
+x1 <- 181000
+y1 <- 331500
 
-x2 <- 1525348.068
-y2 <- 6113380.046
+x2 <- 180000
+y2 <- 330500
 
 dx <- x2 - x1
 dy <- y2 - y1
@@ -28,41 +31,40 @@ angle <- atan(dx/dy)
 
 ##	Now, find the endpoints of the line, with that angle passing through our 
 ##		Hu line but ending with the N and S transect y values:
-new_y <- 6451870
+new_y <- 332140
 dy <- new_y - y2
 dx <- tan(angle) * dy
 new_x <- x2 + dx
-Hu_N_transect <- list(x=new_x, y=new_y)
+N_transect <- list(x=new_x, y=new_y)
 
-new_y <- 2317627
-dy <- Hu_N_transect$y - new_y
+new_y <- 330460
+dy <- N_transect$y - new_y
 dx <- tan(angle) * dy
-new_x <- Hu_N_transect$x - dx
-Hu_S_transect <- list(x=new_x, y=new_y)
+new_x <- N_transect$x - dx
+S_transect <- list(x=new_x, y=new_y)
 
 
 ##	So, to create a series of lines that have a distance perpendicular to
 ##		each other equal to the slice_width, the next one to the west is:
 dx <- (1 / cos(angle)) * slice_width
-new_x1 <- Hu_S_transect$x - dx
-new_y1 <- Hu_S_transect$y
-new_x2 <- Hu_N_transect$x - dx
-new_y2 <- Hu_N_transect$y
+new_x1 <- S_transect$x - dx
+new_y1 <- S_transect$y
+new_x2 <- N_transect$x - dx
+new_y2 <- N_transect$y
 
 
 ##	Create series of lines based on dx and the coordinates along transects:
 ##		Based on solution at StackOverflow.com:
 ##			http://stackoverflow.com/questions/20531066/convert-begin-and-end-coordinates-into-spatial-lines-in-r
-require(sp)
 
 ##	Raw list to store Lines objects
-x2_transect <- c( seq(from=Hu_N_transect$x, to=3903000, by=dx), seq(from=Hu_N_transect$x - dx, to=-1658000, by=-1*dx) )
-y2_transect <- rep(Hu_N_transect$y, length(x2_transect))
+x2_transect <- c( seq(from=N_transect$x, to=3903000, by=dx), seq(from=N_transect$x - dx, to=-1658000, by=-1*dx) )
+y2_transect <- rep(N_transect$y, length(x2_transect))
 
-dy_trans <- Hu_N_transect$y - Hu_S_transect$y
+dy_trans <- N_transect$y - S_transect$y
 dx_trans <- tan(angle) * dy_trans
 x1_transect <- x2_transect - dx_trans
-y1_transect <- rep(Hu_S_transect$y, length(x2_transect))
+y1_transect <- rep(S_transect$y, length(x2_transect))
 
 begin.coord <- data.frame("x"=x1_transect, "y"=y1_transect)
 begin.coord <- begin.coord[order(begin.coord$x),]
@@ -79,8 +81,6 @@ row.names(begin.coord) <- as.character(1:nrow(begin.coord))
 row.names(end.coord) <- as.character(1:nrow(end.coord))
 
 
-require(sp)
-
 l <- vector("list", nrow(begin.coord))
 
 for (i in seq_along(l)) {
@@ -89,37 +89,32 @@ for (i in seq_along(l)) {
 
 sl <- SpatialLines(l)
 
-##	Load our Census Shapefile to pull coordinate system from:
-require(rgdal)
-census <- readOGR(dsn="data", layer="census_covariates")
 
+##	Plot to confirm locations:
+coordinates(meuse.grid) <- ~x+y
+gridded(meuse.grid) <- TRUE
+proj4string(meuse.grid) <- CRS(paste("+init=epsg:28992","+towgs84=565.237,50.0087,465.658,-0.406857,0.350733,-1.87035,4.0812"))
+plot(meuse.grid)
+lines(sl)
+
+##	Load our Census Shapefile to pull coordinate system from:
 names(begin.coord) <- c("begin_x", "begin_y")
 names(end.coord) <- c("end_x", "end_y")
 sldf <- SpatialLinesDataFrame(sl, data.frame("lineID"=1:nrow(begin.coord), begin.coord, end.coord))
 
 ##	Set CRS from existing census file:
-proj4string(sldf) <- proj4string(census)
-
-##	Write it out:
-writeOGR(sldf, "output", "hu_lines", driver="ESRI Shapefile", check_exists=TRUE, overwrite_layer=TRUE)
-
+proj4string(sldf) <- proj4string(meuse.grid)
 
 
 ##	Do the slicing using RGEOS as the ArcGIS method is extremely slow.  Note that this produces
 ##		polygons whose boundaries are 0.000002 meters separated so they are not ideal for 
 ##		further processing but they should work ok for our purposes:
 ##			http://stackoverflow.com/questions/5726297/cut-polygons-using-contour-line-beneath-the-polygon-layers
-require(rgeos)
-
-census_diss <- readOGR(dsn="data", layer="census_covariates_diss")
-#lpi <- gIntersection(census_diss, sldf)
-#blpi <- gBuffer(lpi, width = 0.000001)
-#dpi <- gDifference(census_diss, blpi)
-#dpi_df <- SpatialPolygonsDataFrame(dpi, data.frame("diss"=1))
-#writeOGR(dpi_df, "output", "census_covariates_diss_slices", driver="ESRI Shapefile", check_exists=TRUE, overwrite_layer=TRUE)
-
-##	The above works but creates a single multi-part polygon output.  Insteac, we'll
-##		grow the lines to have a buffer that matches the distance between lines...
 blpi <- gBuffer(sldf, byid=TRUE, id=sldf$lineID, width = slice_width/2)
-writeOGR(blpi, "output", "census_covariates_diss_slices", driver="ESRI Shapefile", check_exists=TRUE, overwrite_layer=TRUE)
+plot(blpi, col="red", add=T)
 
+
+##	If necessary, write out lines and buffers:
+
+#writeOGR(sldf, ".", "lines", driver="ESRI Shapefile", check_exists=TRUE, overwrite_layer=TRUE)
+#writeOGR(blpi, ".", "slices", driver="ESRI Shapefile", check_exists=TRUE, overwrite_layer=TRUE)
